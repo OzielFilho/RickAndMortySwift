bbfa0e0ceb7775f4378a16377b16480c8afd8210
OzielFilho <ozielfilho03@hotmail.com>
2025-02-19 11:07:45 -0300

    include pagination in home view for fetch more characters

---
+++ RickAndMorty/Domain/Entity/RickAndMortyCharacterInfo.swift
@@ -0,0 +1,13 @@
+//
+//  RickAndMortyInfo.swift
+//  RickAndMorty
+//
+//  Created by Oziel Pontes on 19/02/25.
+//
+
+import Foundation
+
+struct RickAndMortyInfo: Decodable, Equatable {
+    let count: Int
+    let pages: Int
+}
